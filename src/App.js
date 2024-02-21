4b1f28673ee5ab87860760344bab1b7cc446073a
prashanth <reddyprashanth512@gmail.com>
2024-02-21 10:22:39 +0000

    solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // App.js
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 import EmojiGame from './components/EmojiGame'
 import MemoryMatrix from './components/MemoryMatrix'
 import Home from './components/Home'
@@ -7,18 +7,12 @@ import GamingComponents from './components/GamingComponents'
 import './App.css'
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/emoji/game" component={EmojiGame} />
-      <Route exact path="/memory/game" component={MemoryMatrix} />
-      <Route
-        exact
-        path="/rock/paper/scissor/game"
-        component={GamingComponents}
-      />
-    </Switch>
-  </BrowserRouter>
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/emoji/game" component={EmojiGame} />
+    <Route exact path="/memory/game" component={MemoryMatrix} />
+    <Route exact path="/rock/paper/scissor/game" component={GamingComponents} />
+  </Switch>
 )
 
 export default App
--- src/components/MemoryMatrix/index.js
+++ src/components/MemoryMatrix/index.js
@@ -5,8 +5,41 @@ import './index.css'
 class MemoryMatrix extends Component {
   render() {
     return (
-      <div>
-        <h1>Hii </h1>
+      <div className="memory-matrix-container">
+        <h1 className="game-heading">Memory Matrix</h1>
+        <div className="level-container">
+          <p className="level">Level-1</p>
+          <p className="level">Max Level-00</p>
+        </div>
+        <div className="game-container">
+          <button type="button" className="button">
+            .
+          </button>
+          <button type="button" className="button">
+            .
+          </button>
+          <button type="button" className="button">
+            .
+          </button>
+          <button type="button" className="button">
+            .
+          </button>
+          <button type="button" className="button">
+            .
+          </button>
+          <button type="button" className="button">
+            .
+          </button>
+          <button type="button" className="button">
+            .
+          </button>
+          <button type="button" className="button">
+            .
+          </button>
+          <button type="button" className="button">
+            .
+          </button>
+        </div>
       </div>
     )
   }
